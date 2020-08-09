854fced7580bbafbd2151ff8b1d6c474b7241001
Pansystar <wh.bt3.p@gmail.com>
2020-08-09 18:35:59 +0900

    create template.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def Hello():
     name = "pansystar"
-    return app.send_static_file('/index.html')
+    return render_template('index.html')
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
