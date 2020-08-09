28e3659314dffeec06d36b10fd54b1337ad52a99
Pansystar <wh.bt3.p@gmail.com>
2020-08-09 18:41:59 +0900

    create get post method

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 import os
 
 app = Flask(__name__)
 
 
+
 @app.route("/")
-def Hello():
-    name = "pansystar"
-    return render_template('index.html')
+def get_method():
+    return render_template('index.html', name="")
+
+@app.route("/send", methods=["GET"])
+def get_method():
+    name = "GET!!!"
+    return render_template('index.html', name=name)
+
+
+@app.route("/send", methods=["POST"])
+def get_method():
+    name = "GET!!!"
+    return render_template('index.html', name=name)
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
