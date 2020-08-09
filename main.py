5d2f89aab6bd64324ced49d15708c9d7885dbb4d
Pansystar <wh.bt3.p@gmail.com>
2020-08-09 22:09:17 +0900

    create database get

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, jsonify
+import pandas as pd
+from sqlalchemy import create_engine
 import os
 
 app = Flask(__name__)
@@ -7,7 +9,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template('index.html', name="")
+    name = data_select()
+    return render_template('index.html', name=name)
 
 @app.route("/send", methods=["GET"])
 def get_method():
@@ -23,3 +26,8 @@ def post_method():
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
+def data_select():
+    engine = create_engine('postgres://ozqqhdmuqezsca:a220c425e3f1a1ab567bdfb15625642cd33f3ca924a3846352923f45bf39d6ba@ec2-3-208-50-226.compute-1.amazonaws.com:5432/d4p1h3ane0mls4')  
+    df = pd.read_sql(sql='select * from pansydb;', con=engine)
+    return df
