d478f812caa9a07746da55aaad99707ad2d71641
Pansystar <wh.bt3.p@gmail.com>
2020-08-10 00:43:18 +0900

    repair requirements.txt

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def post_method():
     name = request.form['name']
 
     engine = create_engine('postgres://ozqqhdmuqezsca:a220c425e3f1a1ab567bdfb15625642cd33f3ca924a3846352923f45bf39d6ba@ec2-3-208-50-226.compute-1.amazonaws.com:5432/d4p1h3ane0mls4')  
-    df = pd.read_sql(sql='insert into pansydb (id, user_name, age) values (' + id + ',' + name + ',' + age +');', con=engine)
+    df = pd.read_sql(sql='insert into pansydb (id, user_name, age) values (' + id + ',\'' + name + '\',' + age +');', con=engine)
     
     return render_template('index.html', name=name)
 
