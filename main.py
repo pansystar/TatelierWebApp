1a22c5a0136dba491a5582e3d832cc58e32e39da
Pansystar <wh.bt3.p@gmail.com>
2020-08-15 17:30:16 +0900

    repair insert

--- main.py
+++ main.py
@@ -26,9 +26,16 @@ def post_method():
     name = request.form['name']
 
     engine = create_engine('postgres://ozqqhdmuqezsca:a220c425e3f1a1ab567bdfb15625642cd33f3ca924a3846352923f45bf39d6ba@ec2-3-208-50-226.compute-1.amazonaws.com:5432/d4p1h3ane0mls4')  
-    # df = pd.read_sql(sql='insert into pansydb (id, user_name, age) values ('+ iD +', \'' + name + '\', '+ age +');', con=engine)
     
-    return render_template('index.html', name="OK")
+    arg = ""
+
+    try:
+        df = pd.read_sql(sql='insert into pansydb (id, user_name, age) values ('+ iD +', \'' + name + '\', '+ age +');', con=engine)
+        arg = "OK"
+    except e:
+        arg = "except"
+    
+    return render_template('index.html', name=arg)
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
